0b319e871556cad1b73a6ccb152853d0334dfcdc
koddenbrock <koddenbrock@gmail.com>
2024-12-22 00:03:46 +0100

    added several experiments

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import glob
 import os
 
-import wandb
-
+from cellpose_adapt.utils import set_all_seeds
 from experiments.eval import plot_eval
 from experiments.optimize import optimize_parameters
-from cellpose_adapt.utils import set_all_seeds
-
 
 if __name__ == "__main__":
 
@@ -36,7 +33,6 @@ if __name__ == "__main__":
 
         # Set the random seed based on the image index
         set_all_seeds(42)
-        log_wandb = False
         main_folder = "data/P013T/"
 
         # get all the subfolder
@@ -48,28 +44,54 @@ if __name__ == "__main__":
 
         image_paths = [image_paths[5]]
 
-        # get all the subfolder
-        subfolders = glob.glob(os.path.join(main_folder, "*"))
+        # Define the result files
         result_file_models = os.path.join(main_folder, "results_models.csv")
         result_file_normalize = os.path.join(main_folder, "results_normalize.csv")
+        result_file_diameter = os.path.join(main_folder, "results_diameter.csv")
+        result_file_flow_threshold = os.path.join(main_folder, "results_flow_threshold.csv")
+        result_file_cellprob_threshold = os.path.join(main_folder, "results_cellprob_threshold.csv")
+        result_file_min_size = os.path.join(main_folder, "results_min_size.csv")
+        result_file_stitch_threshold = os.path.join(main_folder, "results_stitch_threshold.csv")
+        result_file_tile_overlap = os.path.join(main_folder, "results_tile_overlap.csv")
 
         for image_idx, image_path in enumerate(image_paths):
-            image_name = os.path.basename(image_path).replace(".tif", "")
 
-            if log_wandb:
-                wandb.init(project="organoid_segmentation", name=f"{image_name}")
 
             # TODO check for other models like "bact_omni"
             options_model = {
                 "model_name": ["cyto2_cp3", "cyto", "cyto2", "cyto3", "nuclei", "tissuenet_cp3", "livecell_cp3",
                                "yeast_PhC_cp3", "yeast_BF_cp3", "bact_phase_cp3", "bact_fluor_cp3", "deepbacs_cp3"]}
-            optimize_parameters(options_model, image_path, result_file_models)
+            optimize_parameters(options_model, image_path, result_file_models, append_result=image_idx > 0)
 
-            options_normalization = {"normalization_min": [0.5, 1, 2, 3, 5, 7, 10]}
-            optimize_parameters(options_normalization, image_path, result_file_normalize)
+            options_normalization = {"normalization_min": [0, 0.5, 1, 2, 3, 5, 7, 10], "normalize": [True, False]}
+            optimize_parameters(options_normalization, image_path, result_file_normalize, append_result=image_idx > 0)
 
-            options_normalization = {"normalization_max": [90, 93, 95, 97, 98, 99, 99.5]}
-            optimize_parameters(options_normalization, image_path, result_file_normalize)
+            options_normalization = {"normalization_max": [90, 93, 95, 97, 98, 99, 99.5, 100], "normalize": [True, False]}
+            optimize_parameters(options_normalization, image_path, result_file_normalize, append_result=True)
+
+            options_diameter = {"diameter": [5, 10, 12, 17, 30, 40, 50, 70, 100]}
+            optimize_parameters(options_diameter, image_path, result_file_diameter, append_result=image_idx > 0)
+
+            options_flow_threshold = {"flow_threshold": [0.01, 0.05, 0.1, 0.2, 0.3, 0.5, 0.7]}
+            optimize_parameters(options_flow_threshold, image_path, result_file_flow_threshold, append_result=image_idx > 0)
+
+            options_cellprob_threshold = {"cellprob_threshold": [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]}
+            optimize_parameters(options_cellprob_threshold, image_path, result_file_cellprob_threshold, append_result=image_idx > 0)
+
+            options_min_size = {"min_size": [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]}
+            optimize_parameters(options_min_size, image_path, result_file_min_size, append_result=image_idx > 0)
+
+            options_stitch_threshold = {"stitch_threshold": [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]}
+            optimize_parameters(options_stitch_threshold, image_path, result_file_stitch_threshold, append_result=image_idx > 0)
+
+            options_tile_overlap = {"tile_overlap": [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]}
+            optimize_parameters(options_tile_overlap, image_path, result_file_tile_overlap, append_result=image_idx > 0)
 
         plot_eval(result_file_models)
         plot_eval(result_file_normalize)
+        plot_eval(result_file_diameter)
+        plot_eval(result_file_flow_threshold)
+        plot_eval(result_file_cellprob_threshold)
+        plot_eval(result_file_min_size)
+        plot_eval(result_file_stitch_threshold)
+        plot_eval(result_file_tile_overlap)
