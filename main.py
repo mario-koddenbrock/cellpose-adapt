0a1a7a1ef48780f579e0c002ec608e836046cb8d
koddenbrock <koddenbrock@gmail.com>
2024-12-28 00:20:35 +0100

    refactoring sbatch into separate scripts

--- main.py
+++ main.py
@@ -3,18 +3,15 @@ from experiments import setup
 
 
 def main():
-    # Set the random seed based on the image index
-    set_all_seeds(42)
-    main_folder = "results/P013T/"
 
-    setup.run_model_experiments(main_folder)
-    setup.run_channel_experiments(main_folder)
-    setup.run_normalization_experiments(main_folder)
-    setup.run_diameter_experiments(main_folder)
-    setup.run_cellprob_threshold_experiments(main_folder)
-    setup.run_min_size_experiments(main_folder)
-    setup.run_stitch_threshold_experiments(main_folder)
-    setup.run_tile_overlap_experiments(main_folder)
+    setup.run_model_experiments()
+    setup.run_channel_experiments()
+    setup.run_normalization_experiments()
+    setup.run_diameter_experiments()
+    setup.run_cellprob_threshold_experiments()
+    setup.run_min_size_experiments()
+    setup.run_stitch_threshold_experiments()
+    setup.run_tile_overlap_experiments()
 
 if __name__ == "__main__":
     main()
