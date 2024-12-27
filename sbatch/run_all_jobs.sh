d0611044663909445619caaef95128ceeec78e85
koddenbrock <koddenbrock@gmail.com>
2024-12-28 00:24:50 +0100

    batch script to run all slurm jobs at once

---
+++ sbatch/run_all_jobs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sbatch sbatch/cellprob.sbatch
+sbatch sbatch/channel.sbatch
+sbatch sbatch/diameter.sbatch
+sbatch sbatch/min_size.sbatch
+sbatch sbatch/model.sbatch
+sbatch sbatch/normalization.sbatch
+sbatch sbatch/stitch.sbatch
+sbatch sbatch/tile.sbatch
