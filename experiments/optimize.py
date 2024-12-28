e65c7c96d032d9e16517207a9ca315320682b7be
Mario Koddenbrock <koddenbrock@gmail.com>
2024-12-28 23:45:28 +0100

    removed nested for loops

--- experiments/optimize.py
+++ experiments/optimize.py
@@ -1,15 +1,14 @@
 import os
+from itertools import product
 
 import wandb
-from sympy.physics.vector.printing import params
 
-from cellpose_adapt.file_io import load_image_with_gt
-from cellpose_adapt.utils import check_set_gpu
-from experiments.results import ResultHandler
 from cellpose_adapt.config import ensure_default_parameter, CellposeConfig
 from cellpose_adapt.core import EvaluationError
+from cellpose_adapt.file_io import load_image_with_gt
 from cellpose_adapt.main import evaluate_model
 from cellpose_adapt.viz import show_napari
+from experiments.results import ResultHandler
 
 
 def optimize_parameters(
@@ -63,68 +62,79 @@ def optimize_parameters(
         # Get the right channel
         image = image_orig[:, channel_idx, :, :] if image_orig.ndim == 4 else image_orig
 
-        for model_name in params["model_name"]:
-            for channel_segment in params["channel_segment"]:
-                for channel_nuclei in params["channel_nuclei"]:
-                    for channel_axis in params["channel_axis"]:
-                        for invert in params["invert"]:
-                            for normalize in params["normalize"]:
-                                for percentile_min in params["percentile_min"]:
-                                    for percentile_max in params["percentile_max"]:
-                                        for diameter in params["diameter"]:
-                                            for do_3D in params["do_3D"]:
-                                                for flow_threshold in params["flow_threshold"]:
-                                                    for cellprob_threshold in params["cellprob_threshold"]:
-                                                        for interp in params["interp"]:
-                                                            for min_size in params["min_size"]:
-                                                                for max_size_fraction in params["max_size_fraction"]:
-                                                                    for niter in params["niter"]:
-                                                                        for stitch_threshold in params["stitch_threshold"]:
-                                                                            for tile_overlap in params["tile_overlap"]:
-                                                                                for norm3D in params["norm3D"]:
-                                                                                    for sharpen in params["sharpen"]:
-                                                                                        for tile_norm in params["tile_norm"]:
+        # Get all combinations of parameters
+        param_combinations = product(
+            params["model_name"],
+            params["channel_segment"],
+            params["channel_nuclei"],
+            params["channel_axis"],
+            params["invert"],
+            params["normalize"],
+            params["percentile_min"],
+            params["percentile_max"],
+            params["diameter"],
+            params["do_3D"],
+            params["flow_threshold"],
+            params["cellprob_threshold"],
+            params["interp"],
+            params["min_size"],
+            params["max_size_fraction"],
+            params["niter"],
+            params["stitch_threshold"],
+            params["tile_overlap"],
+            params["norm3D"],
+            params["sharpen"],
+            params["tile_norm"]
+        )
 
-                                                                                            config = CellposeConfig(
-                                                                                                cellprob_threshold=cellprob_threshold,
-                                                                                                channel_axis=channel_axis,
-                                                                                                channel_nuclei=channel_nuclei,
-                                                                                                channel_segment=channel_segment,
-                                                                                                diameter=diameter,
-                                                                                                do_3D=do_3D,
-                                                                                                flow_threshold=flow_threshold,
-                                                                                                interp=interp,
-                                                                                                invert=invert,
-                                                                                                max_size_fraction=max_size_fraction,
-                                                                                                min_size=min_size,
-                                                                                                model_name=model_name,
-                                                                                                niter=niter,
-                                                                                                norm3D=norm3D,
-                                                                                                normalize=normalize,
-                                                                                                percentile_max=percentile_max,
-                                                                                                percentile_min=percentile_min,
-                                                                                                sharpen=sharpen,
-                                                                                                stitch_threshold=stitch_threshold,
-                                                                                                tile_norm=tile_norm,
-                                                                                                tile_overlap=tile_overlap,
-                                                                                                type=params["type"],
-                                                                                            )
+        for combination in param_combinations:
+            (
+                model_name, channel_segment, channel_nuclei, channel_axis, invert, normalize,
+                percentile_min, percentile_max, diameter, do_3D, flow_threshold, cellprob_threshold,
+                interp, min_size, max_size_fraction, niter, stitch_threshold, tile_overlap,
+                norm3D, sharpen, tile_norm
+            ) = combination
 
-                                                                                            results = evaluate_model(image_name, image, ground_truth, config, cache_dir)
+            config = CellposeConfig(
+                cellprob_threshold=cellprob_threshold,
+                channel_axis=channel_axis,
+                channel_nuclei=channel_nuclei,
+                channel_segment=channel_segment,
+                diameter=diameter,
+                do_3D=do_3D,
+                flow_threshold=flow_threshold,
+                interp=interp,
+                invert=invert,
+                max_size_fraction=max_size_fraction,
+                min_size=min_size,
+                model_name=model_name,
+                niter=niter,
+                norm3D=norm3D,
+                normalize=normalize,
+                percentile_max=percentile_max,
+                percentile_min=percentile_min,
+                sharpen=sharpen,
+                stitch_threshold=stitch_threshold,
+                tile_norm=tile_norm,
+                tile_overlap=tile_overlap,
+                type=params["type"],
+            )
 
-                                                                                            if results == EvaluationError.GROUND_TRUTH_NOT_AVAILABLE:
-                                                                                                break
-                                                                                            elif not isinstance(results, dict):
-                                                                                                continue
+            results = evaluate_model(image_name, image, ground_truth, config, cache_dir)
 
-                                                                                            result_handler.log_result(results, config)
+            if results == EvaluationError.GROUND_TRUTH_NOT_AVAILABLE:
+                break
+            elif not isinstance(results, dict):
+                continue
 
-                                                                                            if show_viewer:
-                                                                                                show_napari(results, config)
+            result_handler.log_result(results, config)
 
-                                                                                            # if results["jaccard"] > 0.95:
-                                                                                            #     print(f"Found good parameters for {type} on {image_path}")
-                                                                                            #     return
+            if show_viewer:
+                show_napari(results, config)
+
+            # if results["jaccard"] > 0.95:
+            #     print(f"Found good parameters for {type} on {image_path}")
+            #     return
 
     if log_wandb:
         wandb.finish()
