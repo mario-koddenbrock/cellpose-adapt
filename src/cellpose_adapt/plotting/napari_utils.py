fa2945d2aeb6a5c1a97c6c492c489e99e171e365
koddenbrock <koddenbrock@gmail.com>
2025-07-25 09:46:01 +0200

    feat: remove unused image and labels layer functions from Napari utilities

--- src/cellpose_adapt/plotting/napari_utils.py
+++ src/cellpose_adapt/plotting/napari_utils.py
@@ -1,8 +1,6 @@
 import logging
 
 import napari
-import numpy as np
-from napari.utils.colormaps import Colormap, ColorArray
 
 
 def show_napari(image, ground_truth, masks, metrics):
@@ -24,15 +22,5 @@ def show_napari(image, ground_truth, masks, metrics):
     logging.info("Launching Napari viewer. Close the window to exit.")
     napari.run()
 
-def add_image_layer(viewer: napari.Viewer, image: np.ndarray, **kwargs):
-    """Adds an image layer to the Napari viewer."""
-    viewer.add_image(image, **kwargs)
 
-def add_labels_layer(viewer: napari.Viewer, labels: np.ndarray, **kwargs):
-    """Adds a labels layer to the Napari viewer."""
-    # Example of setting a default color map if not provided
-    if "colormap" not in kwargs:
-        colors = ColorArray(np.array([[0, 0, 0, 0], [0.4, 0.7, 1, 1]])) # Transparent background, light blue foreground
-        kwargs["colormap"] = Colormap(colors, name="custom_blue")
 
-    viewer.add_labels(labels, **kwargs)
