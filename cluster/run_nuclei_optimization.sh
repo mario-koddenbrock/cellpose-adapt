0269b28d1040e435128ba28e13eb898faad62953
Mario Koddenbrock <koddenbrock@gmail.com>
2025-11-14 10:38:37 +0100

    feat: write log and err in same file

--- cluster/run_nuclei_optimization.sh
+++ cluster/run_nuclei_optimization.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 CONFIGS=(
-  "configs/project_configs/organoid_3d_nuclei_20231108.json"
-  "configs/project_configs/organoid_3d_nuclei_20240220.json"
-  "configs/project_configs/organoid_3d_nuclei_20240305.json"
-  "configs/project_configs/organoid_3d_nuclei_20240701.json"
+  "configs/project_configs/organoid_3d_nuclei_20231108_cluster.json"
+  "configs/project_configs/organoid_3d_nuclei_20240220_cluster.json"
+  "configs/project_configs/organoid_3d_nuclei_20240305_cluster.json"
+#  "configs/project_configs/organoid_3d_nuclei_20241009_cluster.json"
+#  "configs/project_configs/organoid_3d_nuclei_20241023_cluster.json"
 )
 
 for config in "${CONFIGS[@]}"; do
@@ -13,8 +14,8 @@ for config in "${CONFIGS[@]}"; do
   echo "Submitting job for config: $config"
   sbatch \
     --job-name="opt_${shortname}" \
-    --output="logs/${shortname}_%j.out" \
-    --error="logs/${shortname}_%j.err" \
+    --output="logs/${shortname}_%j.log" \
+    --error="logs/${shortname}_%j.log" \
     cluster/optimization.sbatch "$config"
 done
 
