c3c7cf64b33edd99810e129526717303580c2d22
Mario Koddenbrock <koddenbrock@gmail.com>
2026-01-16 16:31:57 +0100

    improved logging

--- cluster/run_nuclei_optimization.sh
+++ cluster/run_nuclei_optimization.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-CONFIGS=(
-  "configs/project_configs/organoid_3d_nuclei_20231108_cluster.json"
-  "configs/project_configs/organoid_3d_nuclei_20240220_cluster.json"
-  "configs/project_configs/organoid_3d_nuclei_20240305_cluster.json"
+#CONFIGS=(
+#  "configs/project_configs/organoid_3d_nuclei_20231108_cluster.json"
+#  "configs/project_configs/organoid_3d_nuclei_20240220_cluster.json"
+#  "configs/project_configs/organoid_3d_nuclei_20240305_cluster.json"
 #  "configs/project_configs/organoid_3d_nuclei_20241009_cluster.json"
 #  "configs/project_configs/organoid_3d_nuclei_20241023_cluster.json"
+#)
+CONFIGS=(
+#  "configs/project_configs/nuclei_3d_25x_cluster.json"
+  "configs/project_configs/nuclei_3d_40x_cluster.json"
 )
 
 for config in "${CONFIGS[@]}"; do
@@ -13,9 +17,9 @@ for config in "${CONFIGS[@]}"; do
   shortname=$(basename "$config" .json)
   echo "Submitting job for config: $config"
   sbatch \
-    --job-name="opt_${shortname}" \
-    --output="logs/${shortname}_%j.log" \
-    --error="logs/${shortname}_%j.log" \
+    --job-name="${shortname}" \
+    --output="logs/%j_${shortname}.log" \
+    --error="logs/%j_${shortname}.log" \
     cluster/optimization.sbatch "$config"
 done
 
